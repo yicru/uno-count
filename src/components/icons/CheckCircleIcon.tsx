f03ead3b22093dd8fa37d2004779c1b9a277eae0
miyamae <yu1ro.mya@gmail.com>
2020-10-30 00:17:31 +0900

    add: CheckCircleIcon

---
+++ src/components/icons/CheckCircleIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import clsx from 'clsx'
+
+type Props = {
+  className?: string
+}
+
+export const CheckCircleIcon: React.FC<Props> = ({ className }) => {
+  return (
+    <svg
+      className={clsx(className)}
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  )
+}
